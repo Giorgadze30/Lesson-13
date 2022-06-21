f0148809b575a4e3653762feb4426287fa59d30d
Giorgadze30 <104830094+Giorgadze30@users.noreply.github.com>
2022-06-21 14:08:12 +0400

    add index.html

---
+++ app.js
@@ -0,0 +1,29 @@
+var userName = Sandro;
+userName = "Example";
+
+let userName = "Saba";
+let userEmail,userAge,userCountry;
+const minAge = 18;
+
+let userName = "Saba"
+    userAge = 21
+    userCountry = "Georgia"
+    userEmail = "sabagiorgadze30@gmail.com"
+
+let myHobby = "Drift"
+    
+let firsttext =
+  "My name is " +
+  myName +
+  ", " +
+  "I am " +
+  (currentYear - yearOfBirth) +
+  " myHobby and Age is" +
+  myHobby +
+  ".";
+console.log(firsttext);
+
+let secondtext = `My name is ${myName}, I am ${
+  currentYear - yearOfBirth
+} myHobby and Age is ${myHobby}.`;
+console.log(secondtext);
